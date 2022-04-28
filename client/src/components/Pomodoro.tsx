856794c7fccb144192923e626850e7e3f6fefca4
Facundo Gimenez <facu-judoca@hotmail.com.ar>
2022-04-28 09:58:45 -0300

    progress bar and clock

--- client/src/components/Pomodoro.tsx
+++ client/src/components/Pomodoro.tsx
@@ -1,27 +1,47 @@
-import React, { useState } from 'react'
+import React, { MouseEvent, useState } from 'react'
 import { BsGearFill } from 'react-icons/bs'
 import styles from "./pomodoro.module.css"
 import PlayIMG from "../assets/images/Play_icon.png"
-type PomodoroState = "inactive" | "config" | "active"
+import ProgressBar from './ProgressBar'
 
+type PomodoroState = "inactive" | "config" | "starting" | "start"
+type PomodoroPage = "home" | "clock"
 const Pomodoro = () => {
   const [pomodoroState,setPomodoroState] = useState<PomodoroState>("inactive")
-  
+  const [pomodoroPage,setPomodoroPage] = useState<PomodoroPage>("home")
+
+  const startGame = () => {
+    setPomodoroPage("clock")
+    setPomodoroState("starting")
+  }
+
   return (
     <div className={styles.pomodoro}>
         <div className={styles.data}>
-
         </div>
         <div className={styles.main_part}>
-                <button className={styles.start_button}>
-                    <img src={PlayIMG} alt="" />  
-                </button>
+            {pomodoroPage == "home" &&
+               <>
+                    <div className={`${styles.clock} ${pomodoroState == "inactive" ? styles.start_button: styles.pomodoro_clock} ` } onClick={() => {startGame()} }>
+                        <img src={PlayIMG} alt="" />
+                    </div>
+               </>
+            }
+            {
+                pomodoroPage == "clock" &&
+                <>
+                <div className={styles.clock}>
+                    <ProgressBar percentage={75}/>
+                </div>
+                </>
+            }
+        </div>
+        <div className={styles.controller}>
+            {pomodoroPage == "home" &&
                 <div>
                     <BsGearFill className={styles.config_icon}/>
                 </div>
-        </div>
-        <div className={styles.controler}>
-
+            }
         </div>
     </div>
     )
