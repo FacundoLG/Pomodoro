db07750637e5b2f6dfffa863c1ddbc7c6243f9bb
Facundo Gimenez <facu-judoca@hotmail.com.ar>
2022-04-28 17:23:09 -0300

    uwu

--- client/src/components/Pomodoro.tsx
+++ client/src/components/Pomodoro.tsx
@@ -1,19 +1,62 @@
-import React, { MouseEvent, useState } from 'react'
+import { FC, useEffect, useState } from 'react'
 import { BsGearFill } from 'react-icons/bs'
 import styles from "./pomodoro.module.css"
 import PlayIMG from "../assets/images/Play_icon.png"
 import ProgressBar from './ProgressBar'
 
-type PomodoroState = "inactive" | "config" | "starting" | "start"
 type PomodoroPage = "home" | "clock"
-const Pomodoro = () => {
-  const [pomodoroState,setPomodoroState] = useState<PomodoroState>("inactive")
-  const [pomodoroPage,setPomodoroPage] = useState<PomodoroPage>("home")
 
+interface Stage {
+    name: "Long break" | "Short break" | "Pomodoro"
+    duration_seg: number
+    status: "active" | "waiting" | "finished"
+}
+
+const stagesMock: Array<Stage> = [
+    {
+        name: "Pomodoro",
+        duration_seg: 1800,
+        status: "waiting" 
+    },
+    {
+        name: "Short break",
+        duration_seg: 300,
+        status: "waiting"
+    },
+    {
+        name: "Pomodoro",
+        duration_seg: 1800,
+        status: "waiting"
+    },
+    {
+        name: 'Long break',
+        duration_seg: 600,
+        status: "waiting"
+    },
+]
+
+const Pomodoro:FC = () => {
+  const [pomodoroPage,setPomodoroPage] = useState<PomodoroPage>("home")
+  const [stages,setStages] = useState<Array<Stage>>(stagesMock)
+  const [clockTime,setClockTime] = useState<string>("")
   const startGame = () => {
-    setPomodoroPage("clock")
-    setPomodoroState("starting")
+      setPomodoroPage("clock")
+      startClock(stages)
+}
+  const startClock = (stages:Array<Stage>) => {
+    stages.map((stage) => {
+        if(stage.status !== "waiting"){
+            let segs = stage.duration_seg
+             setInterval(() => {    
+                const minutes = Math.floor(segs / 60)
+                console.log(segs / 60)
+                setClockTime((minutes).toString() )
+                segs--
+            },1000)
+        }
+    })
   }
+
 
   return (
     <div className={styles.pomodoro}>
@@ -22,7 +65,7 @@ const Pomodoro = () => {
         <div className={styles.main_part}>
             {pomodoroPage == "home" &&
                <>
-                    <div className={`${styles.clock} ${pomodoroState == "inactive" ? styles.start_button: styles.pomodoro_clock} ` } onClick={() => {startGame()} }>
+                    <div className={`${styles.clock} ${ styles.start_button} ` } onClick={() => {startGame()} }>
                         <img src={PlayIMG} alt="" />
                     </div>
                </>
@@ -31,7 +74,7 @@ const Pomodoro = () => {
                 pomodoroPage == "clock" &&
                 <>
                 <div className={styles.clock}>
-                    <ProgressBar percentage={75}/>
+                    <ProgressBar percentage={90} minutes={clockTime} seconds={"00"}/>
                 </div>
                 </>
             }
@@ -40,6 +83,14 @@ const Pomodoro = () => {
             {pomodoroPage == "home" &&
                 <div>
                     <BsGearFill className={styles.config_icon}/>
+                </div>
+            }
+            {
+                pomodoroPage == "clock" &&
+                <div className={styles.stages}>
+                    {stagesMock.map((_,i) => 
+                        <div key={i+ "stage"}></div>
+                    )}
                 </div>
             }
         </div>
