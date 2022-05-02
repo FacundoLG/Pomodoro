e8790c15b18e3e603e5e83eb401361661d014308
Facundo Gimenez <facu-judoca@hotmail.com.ar>
2022-05-02 10:42:55 -0300

    sounds, title as timer, favicon

--- client/src/components/Pomodoro.tsx
+++ client/src/components/Pomodoro.tsx
@@ -1,9 +1,15 @@
+/* Components */
 import { FC, useEffect, useState } from 'react'
 import { BsGearFill } from 'react-icons/bs'
-import styles from "./pomodoro.module.css"
+/* IMG - SVG */
 import PlayIMG from "../assets/images/Play_icon.png"
 import ProgressBar from './ProgressBar'
-
+/* Sounds */
+import Sound from "../assets/sounds/sound-1.wav"
+import Sound2 from "../assets/sounds/sound-2.wav"
+import Sound3 from "../assets/sounds/sound-3.wav"
+/* Styles */
+import styles from "./pomodoro.module.css"
 type PomodoroPage = "home" | "clock"
 
 interface Stage {
@@ -27,24 +33,44 @@ interface ClockData {
 const stagesMock: Array<Stage> = [
     {
         name: "Pomodoro",
-        duration_sec: 10,
+        duration_sec: 1500,
         status: "waiting" 
     },
     {
         name: "Short break",
-        duration_sec: 10,
+        duration_sec: 300,
         status: "waiting"
     },
     {
         name: "Pomodoro",
-        duration_sec: 20,
+        duration_sec: 1500,
+        status: "waiting"
+    },
+    {
+        name: 'Short break',
+        duration_sec: 300,
+        status: "waiting"
+    },
+    {
+        name: 'Pomodoro',
+        duration_sec: 1500,
+        status: "waiting"
+    },
+    {
+        name: 'Short break',
+        duration_sec: 300,
+        status: "waiting"
+    },
+    {
+        name: 'Pomodoro',
+        duration_sec: 1500,
         status: "waiting"
     },
     {
         name: 'Long break',
-        duration_sec: 10,
+        duration_sec: 1200,
         status: "waiting"
-    },
+    }, 
 ]
 
  
@@ -53,7 +79,6 @@ const Pomodoro:FC = () => {
   const [pomodoroPage,setPomodoroPage] = useState<PomodoroPage>("home")
   const [stages,setStages] = useState<Array<Stage>>(stagesMock)
   const [actualStage,setActualStage] = useState<ActualStageMetaData>()
-  const [isStageFinished,setIsStageFinished] = useState<boolean>(true)
   const [clockData,setClockData] = useState<ClockData>()
   const startGame = () => {
       setPomodoroPage("clock")
@@ -73,13 +98,20 @@ const Pomodoro:FC = () => {
 
 useEffect(() => {
     if (!actualStage) return
+    const start_sound = new Audio(Sound3)
+    start_sound.play()
     let seg = actualStage.stage.duration_sec
     const clockInterval = setInterval(() => {
           if(seg <= 0){
               if(actualStage.stage_index == actualStage.stages_length - 1){
-                  setPomodoroPage("home")   
+                const finish_clock_sound = new Audio(Sound)
+                finish_clock_sound.play()  
+                setPomodoroPage("home")
+                  document.title = "Pomodoro" 
               }
               else{
+                  const finish_stage_sound = new Audio(Sound2)
+                  finish_stage_sound.play()
                    setActualStage({
                           ...actualStage,
                           stage: stages[actualStage.stage_index + 1],
@@ -92,14 +124,16 @@ useEffect(() => {
           console.log(percentage)
           const minutes = Math.floor(seg / 60)
           const seconds = seg - 60 * minutes
+          document.title = `${fix_one_digit(minutes)}:${fix_one_digit(seconds)}-${actualStage.stage.name}`
           setClockData({
               minutes: fix_one_digit(minutes),
               seconds: fix_one_digit(seconds),
               percentage
             })
-            seg--
+          seg--
             return () => {
             clearInterval(clockInterval)
+            setPomodoroPage("clock")
         }
     },1000)
   
@@ -109,6 +143,10 @@ useEffect(() => {
   return (
     <div className={styles.pomodoro}>
         <div className={styles.data}>
+            {
+                pomodoroPage === "clock" &&
+                <p className={styles.stage_name}>{actualStage?.stage.name}</p>   
+            }
         </div>
         <div className={styles.main_part}>
             {pomodoroPage == "home" &&
@@ -121,7 +159,7 @@ useEffect(() => {
             {
                 pomodoroPage == "clock" &&
                 <>
-                <div className={styles.clock}>
+                <div className={`${styles.clock} ${styles.pomodoro_clock}`}>
                     <ProgressBar percentage={clockData?.percentage || 0} minutes={clockData?.minutes} seconds={clockData?.seconds}/>
                 </div>
                 </>
